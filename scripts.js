e20560de979e484bfbc8c56b54094a3af7b71b4a
JetRobertsBCCA <jroberts@basecampcodingacademy.org>
2024-09-10 19:18:09 -0500

    First

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// JavaScript to greet the user
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Welcome to MythologyGuru! Dive into the world of mythology.");
+});
+
+// Additional interactivity can be added here
